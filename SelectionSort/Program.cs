7f7ad07ce0ffd1180cfc7f63160100e80527d6da
Edgar Perez <e.perez@caribemedia.com.do>
2017-09-14 09:30:10 -0400

    Add project files.

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sorting = new SortAlgorithms<int>();
+            var arrayValues = new int[] { 9,5,2,1,7,3,4,6};
+
+            Console.WriteLine("Array Not Organized: " );
+
+            foreach(var value in arrayValues)
+            {
+                Console.WriteLine(value);
+            }
+            sorting.SortSelection(arrayValues);
+
+            Console.WriteLine("Array Already Organized: ");
+
+            foreach (var value in arrayValues)
+            {
+                Console.WriteLine(value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SelectionSort/SortAlgorithms.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    public class SortAlgorithms<T> where T : IComparable<T>
+    {
+        public void SortSelection(T[] items)
+        {
+            int indexInfo = 0;
+            while (indexInfo < items.Length)
+            {
+                int smallestInt = FindSmallestInt(items, indexInfo);
+                var temp = items[indexInfo];
+                items[indexInfo] = items[smallestInt];
+                items[smallestInt] = temp;
+                indexInfo++;
+            }
+        }
+
+        private int FindSmallestInt(T[] items, int indextorganized)
+        {
+            int smallestInt = indextorganized;
+            T ChoosenValue = items[indextorganized];
+            for (int count = indextorganized; count < items.Length; count++)
+            {
+                if (items[count].CompareTo(ChoosenValue) < 0)
+                {
+                    smallestInt = count;
+                    ChoosenValue = items[count];
+                }
+            }
+            return smallestInt;
+        }
+    }
+}
